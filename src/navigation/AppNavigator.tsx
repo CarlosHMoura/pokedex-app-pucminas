6c56871be9517a792092cb1bfca65aa7e5d9f7f9
Isabelle Oliveira <isabelle.caroll.io@gmail.com>
2024-04-13 16:02:47 -0300

    Revert "CRUD Usuário"
    This reverts commit 3138bf4ec09d5ef6735b3823363a373300c387e4.

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 
-import {Home} from "../screens/home";
+import {Home} from "../screens/Home";
 import { PokemonListScreen } from "../screens/PokemonListScreen";
 
 const Stack = createStackNavigator();
